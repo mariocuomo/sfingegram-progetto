b43bee1dc9590e7bcb676825c8b6a25c933c546c
root <cuomomario@hotmail.com>
2021-06-24 19:56:44 +0200

    update start-sfingegram.sh

--- asw-sfingegram/start-sfingegram.sh
+++ asw-sfingegram/start-sfingegram.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 gradle assemble
-docker-compose up -d --scale enigmi=2 --scale enigmi-seguiti=2 --scale connessioni=2
+docker-compose build --no-cache 
+docker-compose up --scale enigmi=2 --scale enigmi-seguiti=2 --scale connessioni=2
